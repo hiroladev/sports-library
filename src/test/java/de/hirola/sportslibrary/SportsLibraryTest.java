64034bc7fa185ce467aa4c48af94b20e2b7d77c2
Michael Schmidt <development@hirola.de>
2022-02-12 14:18:21 +0100

    Add new tests.

--- src/test/java/de/hirola/sportslibrary/SportsLibraryTest.java
+++ src/test/java/de/hirola/sportslibrary/SportsLibraryTest.java
@@ -12,37 +12,37 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 class SportsLibraryTest {
 
     SportsLibrary sportsLibrary;
     DataRepository dataRepository;
-    List<? extends PersistentObject>  runningPlans;
 
     @Test
     void testRelations() {
-        // user has an active running plan
-        RunningPlan runningPlan = new RunningPlan();
-        User user = new User();
-        user.setActiveRunningPlan(runningPlan);
-
-        // 2 location data
-        LocationData locationData1 = new LocationData();
-        LocationData locationData2 = new LocationData();
-        // add to a list
-        List<LocationData> locations = new ArrayList<>(2);
-        locations.add(locationData1);
-        locations.add(locationData2);
-        // create a track with locations
-        Track track = new Track("Test-Track",null, null,locations);
-
-        // create a training with track
-        Training training = new Training("Training", null, null, track, null);
-
         try {
-            // save
+            // user has an active running plan
+            RunningPlan runningPlan = new RunningPlan();
+            User user = new User();
+            user.setActiveRunningPlan(runningPlan);
+
+            // create 2 location data with associated track
+            LocationData locationData1 = new LocationData();
+            LocationData locationData2 = new LocationData();
+
+            // add to a list
+            List<LocationData> locations = new ArrayList<>(2);
+            locations.add(locationData1);
+            locations.add(locationData2);
+
+            // create a track with locations
+            Track track = new Track("Test-Track",null, locations);
+
+            // create a training with track
+            Training training = new Training("Training", null, null, track, null);
+
+            // save all objects
             dataRepository= new DataRepository(null);
             dataRepository.save(runningPlan);
             dataRepository.save(user);
@@ -100,7 +100,7 @@ class SportsLibraryTest {
 
             // test the import from the templates
             // exists 3 running plans in local datastore?
-            runningPlans = dataRepository.findAll(RunningPlan.class);
+            List<? extends PersistentObject> runningPlans = dataRepository.findAll(RunningPlan.class);
             assertEquals(3,runningPlans.size());
 
             // test the correct start date
@@ -179,59 +179,190 @@ class SportsLibraryTest {
     }
 
     @Test
-    void testCRUD() {
+    void testTrackAndLocationsCRUD() {
         try {
             sportsLibrary = new SportsLibrary("SportsLibraryTest", null);
             assertNotNull(sportsLibrary, "Library not initialize.");
             dataRepository = sportsLibrary.getDataRepository();
             assertNotNull(dataRepository, "DataRepository not initialize.");
 
-            /* failure - need to checked
-            // delete an object with relations
+            // create a track with locations
             LocationData locationData1 = new LocationData();
             LocationData locationData2 = new LocationData();
+            String location1UUID = locationData1.getUUID();
+            String location2UUID = locationData2.getUUID();
+
+            // add to a list
             List<LocationData> locations = new ArrayList<>(2);
             locations.add(locationData1);
             locations.add(locationData2);
-
-            // add a track with locations
-            Track track = new Track("Test-Track", null,null,locations);
+            Track track = new Track("Test-Track",null, locations);
             String trackUUID = track.getUUID();
-            PersistentObject object1 = dataRepository.findByUUID(Track.class, trackUUID);
-            assertNotNull(object1, "Track was not saved");
-            List<? extends PersistentObject> objects1 = dataRepository.findAll(LocationData.class);
-            assertEquals(2, objects1.size(), "LocationData not saved");
+
+            // save only the track
+            dataRepository.save(track);
+
+            // checks
+            PersistentObject savedTrack = dataRepository.findByUUID(Track.class, trackUUID);
+            assertNotNull(savedTrack, "Track was not saved");
+            List<? extends PersistentObject> savedLocations = dataRepository.findAll(LocationData.class);
+            assertEquals(2, savedLocations.size(), "LocationData not saved");
+            for (PersistentObject p : savedLocations) {
+                if (!p.getUUID().equals(location1UUID) && !p.getUUID().equals(location2UUID)) {
+                    fail("Different UUID from locations");
+                }
+
+            }
+
             // remove the track, locations should be deleted
             dataRepository.delete(track);
-            PersistentObject object2 = dataRepository.findByUUID(Track.class, trackUUID);
-            assertNull(object2, "Track was not deleted");
-            List<? extends PersistentObject> objects2 = dataRepository.findAll(LocationData.class);
-            assertEquals(0, objects1.size(), "LocationData was not deleted.");
+            PersistentObject deletedTrack = dataRepository.findByUUID(Track.class, trackUUID);
+            assertNull(deletedTrack, "Track was not deleted");
+            List<? extends PersistentObject> deletedLocations = dataRepository.findAll(LocationData.class);
+            assertEquals(0, deletedLocations.size(), "LocationData was not deleted.");
 
-            RunningPlan runningPlan = (RunningPlan) dataRepository.findAll(RunningPlan.class).get(0);
-            RunningPlanEntry entry = runningPlan.getEntries().get(0);
-            assertNotNull(entry);
-            String runningPlanEntryUUID = entry.getUUID();
-            RunningUnit unit = entry.getRunningUnits().get(0);
-            assertNotNull(unit);
-            String unitUUID = unit.getUUID();
-            MovementType movementType = unit.getMovementType();
-            assertNotNull(movementType);
-            String movementTypeUUID = movementType.getUUID();
 
+
+        } catch (SportsLibraryException exception) {
+            fail(exception.getMessage());
+        }
+
+        // delete all objects
+        dataRepository.clearAll();
+        dataRepository.close();
+    }
+
+    @Test
+    void testTrackAndTrainingTypeAndTrainingCRUD() {
+        try {
+            sportsLibrary = new SportsLibrary("SportsLibraryTest", null);
+            assertNotNull(sportsLibrary, "Library not initialize.");
+            dataRepository = sportsLibrary.getDataRepository();
+            assertNotNull(dataRepository, "DataRepository not initialize.");
+
+            // create a track with locations
+            LocationData locationData1 = new LocationData();
+            LocationData locationData2 = new LocationData();
+            String location1UUID = locationData1.getUUID();
+            String location2UUID = locationData2.getUUID();
+
+            // add to a list
+            List<LocationData> locations = new ArrayList<>(2);
+            locations.add(locationData1);
+            locations.add(locationData2);
+            Track track = new Track("Test-Track",null, locations);
+            String trackUUID = track.getUUID();
+
+            // create a training with track
+            Training training = new Training("Test-Training", null, null, track, null);
+            String trainingUUID = training.getUUID();
+            String trainingTypeUUID = training.getTrainingType().getUUID();
+            // save only the training
+            dataRepository.save(training);
+
+            // checks
+            PersistentObject savedTraining = dataRepository.findByUUID(Training.class, trainingUUID);
+            assertNotNull(savedTraining, "Training was not saved");
+            PersistentObject savedTrack = dataRepository.findByUUID(Track.class, trackUUID);
+            assertNotNull(savedTrack, "Track was not saved");
+            List<? extends PersistentObject> savedLocations = dataRepository.findAll(LocationData.class);
+            assertEquals(2, savedLocations.size(), "LocationData not saved");
+            for (PersistentObject p : savedLocations) {
+                if (!p.getUUID().equals(location1UUID) && !p.getUUID().equals(location2UUID)) {
+                    fail("Different UUID from locations");
+                }
+            }
+
+            // remove the training, type of training, track and locations should be NOT deleted
+            dataRepository.delete(training);
+            PersistentObject notDeletedTrack = dataRepository.findByUUID(Track.class, trackUUID);
+            assertNotNull(notDeletedTrack, "Track was deleted");
+            List<? extends PersistentObject> notDeletedLocations = dataRepository.findAll(LocationData.class);
+            assertEquals(2, notDeletedLocations.size(), "LocationData was deleted.");
+            PersistentObject notDeletedTrainingType = dataRepository.findByUUID(TrainingType.class, trainingTypeUUID);
+            assertNull(notDeletedTrainingType, "TrainingType was deleted");
+
+        } catch (SportsLibraryException exception) {
+            fail(exception.getMessage());
+        }
+
+        // delete all objects
+        dataRepository.clearAll();
+        dataRepository.close();
+    }
+
+    @Test
+    void testRunningPlanCRUD() {
+        try {
+            sportsLibrary = new SportsLibrary("SportsLibraryTest", null);
+            assertNotNull(sportsLibrary, "Library not initialize.");
+            dataRepository = sportsLibrary.getDataRepository();
+            assertNotNull(dataRepository, "DataRepository not initialize.");
+
+            // create a running plan
+            // this movement type (with the key 'L') already exists!
+            // saving th running plan updates an existing object
+            MovementType movementType1 = new MovementType("L", "Running", "red", 5, 5);
+            // a new  movement type
+            MovementType movementType2 = new MovementType("Y", "Yoga", "red", 0.0, 0.0);
+
+            RunningUnit runningUnit1 = new RunningUnit(30, movementType1);
+            RunningUnit runningUnit2 = new RunningUnit(5, movementType2);
+            String runningUnit1UUID = runningUnit1.getUUID();
+            String runningUnit2UUID = runningUnit2.getUUID();
+            List<RunningUnit> runningUnits = new ArrayList<>(2);
+            runningUnits.add(runningUnit1);
+            runningUnits.add(runningUnit2);
+
+            RunningPlanEntry runningPlanEntry = new RunningPlanEntry(1,1, runningUnits);
+            String runningPlanEntryUUID = runningPlanEntry.getUUID();
+
+            List<RunningPlanEntry> runningPlanEntries = new ArrayList<>(1);
+            runningPlanEntries.add(runningPlanEntry);
+            RunningPlan runningPlan = new RunningPlan("Test-Plan", null,1,runningPlanEntries, false);
             String runningPlanUUID = runningPlan.getUUID();
+
+            // color is green by default
+            MovementType movementType1beforeUpdated = (MovementType) dataRepository.findByUUID(MovementType.class, "L");
+            assertNotNull(movementType1beforeUpdated, "No movement type with key / uuid 'L'.");
+            assertEquals(Global.Defaults.DEFAULT_MOVEMENT_TYPE_COLOR, movementType1beforeUpdated.getColorKeyString(),
+                    "Default color not " + Global.Defaults.DEFAULT_MOVEMENT_TYPE_COLOR + ".");
+
+            // save only the runningPlan should throw an error because the movement typ with key 'L'
+            dataRepository.save(runningPlan);
+
+            // checks
+            PersistentObject savedRunningPlan = dataRepository.findByUUID(RunningPlan.class, runningPlanUUID);
+            assertNotNull(savedRunningPlan, "RunningPlan was not saved.");
+            PersistentObject savedRunningPlanEntry = dataRepository.findByUUID(RunningPlanEntry.class, runningPlanEntryUUID);
+            assertNotNull(savedRunningPlanEntry, "RunningPlanEntry was not saved.");
+            PersistentObject savedRunningUnit1 = dataRepository.findByUUID(RunningUnit.class, runningUnit1UUID);
+            assertNotNull(savedRunningUnit1, "RunningUnit 1 was not saved.");
+            PersistentObject savedRunningUnit2 = dataRepository.findByUUID(RunningUnit.class, runningUnit2UUID);
+            assertNotNull(savedRunningUnit2, "RunningUnit 2 was not saved.");
+            // movement type with key 'L' has now a new color
+            assertEquals(Global.Defaults.DEFAULT_MOVEMENT_TYPE_COLOR, movementType1beforeUpdated.getColorKeyString(),
+                    "Default color not " + Global.Defaults.DEFAULT_MOVEMENT_TYPE_COLOR + ".");
+            PersistentObject savedMovementType2 = dataRepository.findByUUID(MovementType.class, "Y");
+            assertNotNull(savedMovementType2, "Movement type 2 was not saved.");
+
+
+            // remove the plan, entry and units should be deleted but the movement types not
             dataRepository.delete(runningPlan);
-            PersistentObject object1 = dataRepository.findByUUID(RunningPlan.class, runningPlanUUID);
-            assertNull(object1, "Plan not deleted.");
-            // entry must be deleted, cascade policy is ALL
-            PersistentObject object2 = dataRepository.findByUUID(RunningPlanEntry.class, runningPlanEntryUUID);
-            assertNull(object2,"Entry not deleted.");
-            // unit must be deleted, cascade policy is ALL
-            PersistentObject object3 = dataRepository.findByUUID(RunningUnit.class, unitUUID);
-            assertNull(object3, "Unit not deleted.");
-            // movement type must be not deleted
-            PersistentObject object4 = dataRepository.findByUUID(MovementType.class, movementTypeUUID);
-            assertNotNull(object4, "MovementType deleted."); */
+
+            PersistentObject deletedRunningPlan = dataRepository.findByUUID(RunningPlan.class, runningPlanUUID);
+            assertNull(deletedRunningPlan, "Running plan was not deleted.");
+            PersistentObject deletedRunningPlanEntry = dataRepository.findByUUID(RunningPlanEntry.class, runningPlanEntryUUID);
+            assertNull(deletedRunningPlanEntry, "Entry was not deleted.");
+            PersistentObject deletedRunningPlanUnit1 = dataRepository.findByUUID(RunningUnit.class, runningPlanUUID);
+            assertNull(deletedRunningPlanUnit1, "Unit 1 was not deleted.");
+            PersistentObject deletedRunningPlanUnit2 = dataRepository.findByUUID(RunningUnit.class, runningPlanUUID);
+            assertNull(deletedRunningPlanUnit2, "Unit 2 was not deleted.");
+            PersistentObject movementType1PastDeleted = dataRepository.findByUUID(MovementType.class, "L");
+            assertNotNull(movementType1PastDeleted, "Movement type with key 'L' was deleted.");
+            PersistentObject movementType2PastDeleted = dataRepository.findByUUID(MovementType.class, "Y");
+            assertNotNull(movementType2PastDeleted, "Movement type with key 'Y' was deleted.");
+
 
         } catch (SportsLibraryException exception) {
             fail(exception.getMessage());
