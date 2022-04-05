e5e8bb9ac37535363c92cef5d4f7ef51b567e9fe
Michael Schmidt <development@hirola.de>
2022-04-05 17:20:23 +0200

    Add pulse and pace to running unit.

--- src/main/java/de/hirola/sportslibrary/model/RunningUnit.java
+++ src/main/java/de/hirola/sportslibrary/model/RunningUnit.java
@@ -32,6 +32,9 @@ public class RunningUnit extends PersistentObject {
     private boolean isCompleted;
     private long duration; // duration in minutes
     private MovementType movementType;
+    private int lowerPulseLimit;
+    private int upperPulseLimit;
+    private double pace; //  distance (kilometers) in minutes
 
     /**
      * Default constructor for reflection and database management.
@@ -52,6 +55,9 @@ public class RunningUnit extends PersistentObject {
     public RunningUnit(int duration, @NotNull MovementType movementType) {
         this.duration = duration;
         this.movementType = movementType;
+        lowerPulseLimit = 0;
+        upperPulseLimit = 0;
+        pace = 0.0;
         isCompleted = false;
     }
 
@@ -74,6 +80,84 @@ public class RunningUnit extends PersistentObject {
     }
 
     /**
+     *  Get the duration of the unit in minutes.
+     *
+     * @return The duration of the unit in minutes.
+     */
+    public long getDuration() {
+        return duration;
+    }
+
+    /**
+     * Set the duration of the running unit.
+     *
+     * @param duration of running unit in minutes
+     */
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    /**
+     * Returns the lower limit of the heart rate range during exercise.
+     *
+     * @return The lower limit of the heart rate range during exercise.
+     */
+    public int getLowerPulseLimit() {
+        return lowerPulseLimit;
+    }
+
+    /**
+     * Set the lower limit of the heart rate range during exercise.
+     *
+     * @param lowerPulseLimit of the heart rate range during exercise.
+     */
+    public void setLowerPulseLimit(int lowerPulseLimit) {
+        if (lowerPulseLimit > 0 && lowerPulseLimit > upperPulseLimit) {
+            return;
+        }
+        this.lowerPulseLimit = lowerPulseLimit;
+    }
+
+    /**
+     * Returns the upper limit of the heart rate range during exercise.
+     *
+     * @return The upper limit of the heart rate range during exercise.
+     */
+    public int getUpperPulseLimit() {
+        return upperPulseLimit;
+    }
+
+    /**
+     * Returns the upper limit of the heart rate range during exercise.
+     *
+     * @param upperPulseLimit of the heart rate range during exercise
+     */
+    public void setUpperPulseLimit(int upperPulseLimit) {
+        if (upperPulseLimit > 0 && upperPulseLimit < lowerPulseLimit) {
+            return;
+        }
+        this.upperPulseLimit = upperPulseLimit;
+    }
+
+    /**
+     * Get the pace for the running unit.
+     *
+     * @return The pace of the running unit in minutes for 1 kilometer.
+     */
+    public double getPace() {
+        return pace;
+    }
+
+    /**
+     * Set the pace for the running unit.
+     *
+     * @param pace of the running unit in minutes for 1 kilometer
+     */
+    public void setPace(double pace) {
+        this.pace = pace;
+    }
+
+    /**
      * Get a flag to determine if unit is completed.
      *
      * @return  A flag to determine if unit is completed
@@ -91,21 +175,15 @@ public class RunningUnit extends PersistentObject {
         isCompleted = completed;
     }
 
-    /**
-     * Get the duration of the unit in minutes.
-     *
-     * @return The duration of the unit in minutes.
-     */
-    public long getDuration() {
-        return duration;
-    }
-
     @Override
     public Document write(NitriteMapper mapper) {
         Document document = new Document();
         document.put("uuid", uuid);
         document.put("isCompleted", isCompleted);
         document.put("duration", duration);
+        document.put("lowerPulseLimit", lowerPulseLimit);
+        document.put("upperPulseLimit", upperPulseLimit);
+        document.put("pace", pace);
 
         if (movementType != null) {
             Document movementTypeDocument = movementType.write(mapper);
@@ -121,6 +199,9 @@ public class RunningUnit extends PersistentObject {
             uuid = (String) document.get("uuid");
             isCompleted = (boolean) document.get("isCompleted");
             duration = (long) document.get("duration");
+            lowerPulseLimit = (int) document.get("lowerPulseLimit");
+            upperPulseLimit = (int) document.get("upperPulseLimit");
+            pace = (double) document.get("pace");
 
             Document movementTypeDocument = (Document) document.get("movementType");
             if (movementTypeDocument != null) {
